a0be31c46bb1b8099e898373b2ac9d8d2359019c
NafizUddin <nafizuddin.ctg@gmail.com>
2024-08-29 04:03:01 +0600

    added table in past bookings

--- src/pages/UserPages/pastBooking/PastBooking.tsx
+++ src/pages/UserPages/pastBooking/PastBooking.tsx
@@ -1,10 +1,14 @@
-import { format, isBefore, parseISO, startOfDay } from "date-fns";
+import { format, isBefore, parse, parseISO } from "date-fns";
 import { useGetIndividualBookingQuery } from "../../../redux/features/bookings/bookings.api";
 import Loading from "../../../components/Loading";
 import SectionTitle from "../../../components/SectionTitle";
+import { useState } from "react";
+import { TBooking } from "../../../types/booking.type";
 
 const PastBooking = () => {
   const { data, isLoading } = useGetIndividualBookingQuery(undefined);
+  const [currentPage, setCurrentPage] = useState(1);
+  const dataPerPage = 5;
 
   const today = new Date();
 
@@ -22,6 +26,16 @@ const PastBooking = () => {
     return <Loading />;
   }
 
+  const convertTo12HourFormat = (time24: string): string => {
+    // Parse the 24-hour time string into a Date object
+    const date = parse(time24, "HH:mm", new Date());
+
+    // Format the Date object into a 12-hour time string with AM/PM
+    return format(date, "hh:mm a");
+  };
+
+  console.log(pastBookings);
+
   return (
     <div className="mt-10">
       <SectionTitle
@@ -33,6 +47,47 @@ const PastBooking = () => {
         <h1 className="text-left font-bold text-2xl lg:text-3xl">
           Previous Bookings
         </h1>
+      </div>
+
+      <div
+        style={{
+          scrollbarWidth: "none" /* Firefox */,
+          msOverflowStyle: "none" /* IE and Edge */,
+          overflowY: "auto" /* Enable scrolling */,
+        }}
+        className="overflow-x-auto m-5"
+      >
+        <table className="table">
+          {/* head */}
+          <thead className="text-lg">
+            <tr>
+              <th>No.</th>
+              <th>Service</th>
+              <th>Date</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pastBookings?.length > 0 &&
+              pastBookings?.map((singleBooking: TBooking, index: number) => (
+                <tr key={index} className="rounded-lg">
+                  <th className="text-lg">
+                    {index + 1 + (currentPage - 1) * dataPerPage}
+                  </th>
+                  <td className="text-lg font-semibold">
+                    {singleBooking?.service?.name}
+                  </td>
+                  <td className="text-lg font-semibold">
+                    {singleBooking?.slot?.date}
+                  </td>
+                  <td className="font-semibold text-lg">
+                    {`${convertTo12HourFormat(singleBooking?.slot?.startTime)} -
+                        ${convertTo12HourFormat(singleBooking?.slot?.endTime)}`}
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
