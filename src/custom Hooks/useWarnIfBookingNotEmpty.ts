2b64473bd9eb26a9c8ee7a3202ce77e710f78840
NafizUddin <nafizuddin.ctg@gmail.com>
2024-08-30 16:51:42 +0600

    added warning hook

---
+++ src/custom Hooks/useWarnIfBookingNotEmpty.ts
@@ -0,0 +1,25 @@
+import { useAppSelector } from "../redux/hooks";
+import { useEffect } from "react";
+
+const useWarnIfCartNotEmpty = () => {
+  const { slotInfo } = useAppSelector((state) => state.bookings);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      if (slotInfo.length > 0) {
+        const message =
+          "You have items in your booking. Are you sure you want to leave?";
+        event.returnValue = message; // For most browsers
+        return message; // For older browsers
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [slotInfo]);
+};
+
+export default useWarnIfCartNotEmpty;
